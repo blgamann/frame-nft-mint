ff30d5329e0194ef4ab4c3b0b5aadb94d534bb03
0xdagarn <sgnoo@naver.com>
2024-08-29 13:32:33 +0900

    Set farcaster metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { config } from "@/config/config";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Farcaster Frame - Mint NFT" />
+
+        {/* required properties */}
+        <meta property="fc:frame" content="vNext" />
+        <meta
+          property="fc:frame:image"
+          content={`${config.hostURL}/image.png`}
+        />
+        <meta property="og:image" content={`${config.hostURL}/image.png`} />
+
+        {/* optional properties */}
+        <meta
+          property="fc:frame:post_url"
+          content={`${config.hostURL}/api/mint}`}
+        />
+        <meta property="fc:frame:button:1" content="Mint NFT" />
+      </Head>
       <body>
         <Main />
         <NextScript />
